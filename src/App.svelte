84605bb98b171c85ba108a248ecd1be1a88a1421
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-22 19:30:53 +0200

    Shrunk glasgow

--- src/App.svelte
+++ src/App.svelte
@@ -3,16 +3,32 @@
   import TimeEvent from "./time_event.svelte";
 </script>
 
+<style>
+  .back {
+    background: linear-gradient(135deg, rgba(101,201,255,1) 5%, rgba(255, 255, 255, 1) 15%);
+  }
+</style>
+
 <Tailwind />
 
-<div class="flex flex-col justify-center h-screen md:container md:mx-auto">
-  <img
-    class="flex-1"
-    alt="Cartoon Eerik"
-    src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light" />
-  <TimeEvent
-    role="MSci in Computing"
-    src="https://ukeducationguide.com/wp-content/uploads/2014/08/glasgow.png"
-    alt="University of Glasgow"
-    timeRange="September 2018 - May 2022" />
+<div class = "back">
+  <div class="flex flex-col justify-center h-screen md:container md:mx-auto ">
+    <img
+      class = "h-72"
+      alt="Cartoon Eerik"
+      src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light" />
+    <div class="flex-1">
+      <TimeEvent
+        role="MSci in Computing"
+        src="media/uni.png"
+        alt="University of Glasgow"
+        timeRange="September 2018 - May 2022" />
+    
+      <TimeEvent
+        role="Software Engineer"
+        src="https://upload.wikimedia.org/wikipedia/commons/4/41/Uros-logo-plain.png"
+        alt="UROS"
+        timeRange="June 2019 - August 2019" />
+    </div>
+  </div>
 </div>
--- src/time_event.svelte
+++ src/time_event.svelte
@@ -2,23 +2,18 @@
   export let role;
   export let src;
   export let alt;
-  export let imgStyles = "h-24 mr-4";
   export let timeRange;
 </script>
 
 <div
-  class="max-w-md mx-auto overflow-hidden bg-white shadow-md rounded-xl md:max-w-2xl">
+  class="max-w-md mx-auto mt-4 overflow-hidden bg-white shadow-md rounded-xl md:max-w-2xl">
+  <p class="text-3xl text-center">{timeRange}</p>
   <div class="md:flex">
-    <div class="md:flex-shrink-0">
-      <img class="object-cover w-full h-48 md:w-72" {src} {alt} />
+    <div class="md:flex-shrink-0 ">
+      <img class="object-cover w-1/2 w-full md:w-48 m:4" {src} {alt} />
     </div>
-    <div class="p-8">
-      <div
-        class="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
-        Education
-      </div>
-      <p class="text-3xl text-center">{timeRange}</p>
-      <p class="text-5xl">{role}</p>
+    <div class="items-center justify-center p-8">
+      <p class="text-3xl text-center">{role}</p>
     </div>
   </div>
 </div>
