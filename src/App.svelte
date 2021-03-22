e107f060ca1f6405253bcabc539f6d04b27b7751
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-22 13:12:23 +0200

    Working but slow

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<style global lang="postcss">
+  /* only apply purgecss on utilities, per Tailwind docs */
+  /* purgecss start ignore */
+  @tailwind base;
+  @tailwind components;
+  /* purgecss end ignore */
+
+  @tailwind utilities;
+</style>
+
+<div class="flex flex-col justify-center h-screen md:container md:mx-auto">
+  <img
+    class="flex-1"
+    alt="Cartoon Eerik"
+    src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light" />
+  <div class="flex flex-row items-center justify-around flex-1 w-1/2">
+    <p class = "text-3xl">September 2018 - May 2022</p>
+    <img
+      class="h-36"
+      alt="University of Glasgow"
+      src="https://ukeducationguide.com/wp-content/uploads/2014/08/glasgow.png" />
+    <p class = "text-5xl">MSci in Computing</p>
+  </div>
+</div>
