b6a9ffc8ad199aeab0d8b68a61600e8967c7f0eb
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-22 14:19:55 +0200

    Making work better for me

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,18 @@
-<style global lang="postcss">
-  /* only apply purgecss on utilities, per Tailwind docs */
-  /* purgecss start ignore */
-  @tailwind base;
-  @tailwind components;
-  /* purgecss end ignore */
+<script>
+  import Tailwind from "./Tailwind.svelte";
+  import TimeEvent from "./time_event.svelte";
+</script>
 
-  @tailwind utilities;
-</style>
+<Tailwind />
 
 <div class="flex flex-col justify-center h-screen md:container md:mx-auto">
   <img
     class="flex-1"
     alt="Cartoon Eerik"
     src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light" />
-  <div class="flex flex-row items-center justify-around flex-1 w-1/2">
-    <p class = "text-3xl">September 2018 - May 2022</p>
-    <img
-      class="h-36"
-      alt="University of Glasgow"
-      src="https://ukeducationguide.com/wp-content/uploads/2014/08/glasgow.png" />
-    <p class = "text-5xl">MSci in Computing</p>
-  </div>
+  <TimeEvent
+    role="MSci in Computing"
+    src="https://ukeducationguide.com/wp-content/uploads/2014/08/glasgow.png"
+    alt="University of Glasgow"
+    timeRange="September 2018 - May 2022" />
 </div>
---
+++ src/time_event.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let role;
+  export let src;
+  export let alt;
+  export let imgStyles = "h-24 mr-4";
+  export let timeRange;
+</script>
+
+<div
+  class="max-w-md mx-auto overflow-hidden bg-white shadow-md rounded-xl md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:flex-shrink-0">
+      <img class="object-cover w-full h-48 md:w-72" {src} {alt} />
+    </div>
+    <div class="p-8">
+      <div
+        class="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
+        Education
+      </div>
+      <p class="text-3xl text-center">{timeRange}</p>
+      <p class="text-5xl">{role}</p>
+    </div>
+  </div>
+</div>
