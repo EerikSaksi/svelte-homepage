e148526d2d98a38936c046794962026c252dffc7
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-23 13:19:50 +0200

    Backgro

--- src/time_event.svelte
+++ src/time_event.svelte
@@ -1,19 +1,22 @@
 <script>
+
+	import { slide } from 'svelte/transition';
   export let role;
   export let src;
   export let alt;
   export let timeRange;
+
 </script>
 
 <div
-  class="max-w-md mx-auto mt-4 overflow-hidden bg-white shadow-md rounded-xl md:max-w-2xl">
-  <p class="text-3xl text-center">{timeRange}</p>
-  <div class="md:flex">
-    <div class="md:flex-shrink-0 ">
-      <img class="object-cover w-1/2 w-full md:w-48 m:4" {src} {alt} />
+  class="max-w-md p-2 mx-auto mt-6 overflow-hidden bg-white shadow-md rounded-xl md:max-w-2xl" transition:slide >
+  <p class="text-2xl text-center sm:text-3xl">{timeRange}</p>
+  <div class="items-center justify-center md:flex">
+    <div class="justify-center md:flex-shrink-0">
+      <img class="object-cover w-2/3 md:w-48" {src} {alt} />
     </div>
     <div class="items-center justify-center p-8">
-      <p class="text-3xl text-center">{role}</p>
+      <p class="text-3xl text-center xs:text-sm">{role}</p>
     </div>
   </div>
 </div>
