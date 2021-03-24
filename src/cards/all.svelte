f695abe33bee13c390420c3e1c82138a06db3345
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-24 12:50:21 +0200

    Grid columns

--- src/cards/all.svelte
+++ src/cards/all.svelte
@@ -1,24 +1,56 @@
 <script>
-  let index = 5;
   import OneCard from "./one_card.svelte";
-  let cardData = [
-    { extraClasses: "bg-green-400" },
-    { extraClasses: "bg-yellow-400 -top-4 -left-4" },
-    { extraClasses: "bg-red-400 -top-8 -left-8" },
-    { extraClasses: "bg-black -top-12 -left-12" },
 
-    //react
+  let index = 3;
+  let cardData = [
+
+    { extraClasses: "bg-green-400", 
+      images: [
+        {
+          src: "https://res.cloudinary.com/practicaldev/image/fetch/s--5N472VlU--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://repository-images.githubusercontent.com/65750241/79017180-d4ce-11e9-9955-3f0a7be00c7a",
+          class: "h-20",
+        },
+        {
+          src: "https://toppng.com/uploads/preview/react-native-svg-transformer-allows-you-import-svg-aperture-science-innovators-logo-11562851994zqcpwozsvy.png",
+          class: "h-32",
+        },
+      ],
+    },
+    { extraClasses: "bg-red-400 -top-8 -left-8", 
+
+      images: [
+        {src: "https://material-ui.com/static/logo.png", class: "h-24"},
+        {src: "https://camo.githubusercontent.com/a664defdd5c2ec93a3fbfb51e0f2aaafa5dc57bf1e13aa47456ced037b3cebe8/68747470733a2f2f676574626f6f7473747261702e636f6d2f646f63732f352e302f6173736574732f6272616e642f626f6f7473747261702d6c6f676f2d736861646f772e706e67", class: "h-24"},
+        {src: "https://miro.medium.com/max/800/1*mUISLg4ghf6QYT_f1-cnlg.png"}
+
+      ],
+    },
+    {
+      extraClasses: "bg-black -top-12 -left-12 flex-col",
+      images: [
+        {
+          src: "https://blog.knoldus.com/wp-content/uploads/2019/06/graphql.png",
+          class: "h-20",
+        },
+
+        {
+          src: "media/postgraphile.png",
+          class: "h-12",
+        },
+      ],
+    },
+
     {
       extraClasses: "bg-purple-400 -top-16 -left-16",
       images: [
         {
           src: "https://image.flaticon.com/icons/png/512/1183/1183672.png",
-          imgClasses: "h-20",
+          class: "h-32",
         },
       ],
     },
+    //{ extraClasses: "bg-yellow-400 -top-4 -left-4" },
   ];
-
   const nextCard = () => {
     console.log("ran");
     cardData[index].extraClasses += " opacity-0";
@@ -36,10 +68,7 @@
   <div class="relative flex">
     <div class="relative flex">
       {#each cardData as cd}
-        <OneCard
-          extraClasses={cd.extraClasses}
-          images = {cd.images}
-        />
+        <OneCard extraClasses={cd.extraClasses} images={cd.images} />
       {/each}
     </div>
   </div>
--- src/cards/one_card.svelte
+++ src/cards/one_card.svelte
@@ -4,8 +4,6 @@
 </script>
 
 <div
-  class="absolute h-40 rounded-lg w-72 transform transition-all skew-x-12 -skew-y-12 {extraClasses}">
-  {#each images as ic}
-    <img alt="Card" src={ic.src} class={ic.class} />
-  {/each}
+  class="grid-cols-2 absolute h-40 rounded-lg w-72 transform transition-all skew-x-12 -skew-y-12 {extraClasses}">
+  {#each images as ic}<img alt="Card" src={ic.src} class={ic.class} />{/each}
 </div>
