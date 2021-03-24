ae2111a3389451eddbe9dda0cc7e3ecb4a8400ea
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-24 14:03:20 +0200

    Figured out grid system

--- src/cards/all.svelte
+++ src/cards/all.svelte
@@ -1,48 +1,51 @@
 <script>
   import OneCard from "./one_card.svelte";
 
-  let index = 3;
+  let index = 1;
   let cardData = [
     {
-      extraClasses: "bg-green-400",
+      extraClasses: "bg-green-400 grid-rows-1 grid-cols-2",
       images: [
         {
           src:
             "https://res.cloudinary.com/practicaldev/image/fetch/s--5N472VlU--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://repository-images.githubusercontent.com/65750241/79017180-d4ce-11e9-9955-3f0a7be00c7a",
-          class: "h-20",
         },
         {
           src:
             "https://toppng.com/uploads/preview/react-native-svg-transformer-allows-you-import-svg-aperture-science-innovators-logo-11562851994zqcpwozsvy.png",
-          class: "h-32",
         },
       ],
     },
     {
-      extraClasses: "bg-red-400 -top-8 -left-8",
-
+      extraClasses: "bg-red-400 -top-8 -left-8 grid-rows-2 grid-cols-2",
       images: [
-        { src: "https://material-ui.com/static/logo.png", class: "h-24" },
+        { src: "https://material-ui.com/static/logo.png" },
         {
           src:
             "https://camo.githubusercontent.com/a664defdd5c2ec93a3fbfb51e0f2aaafa5dc57bf1e13aa47456ced037b3cebe8/68747470733a2f2f676574626f6f7473747261702e636f6d2f646f63732f352e302f6173736574732f6272616e642f626f6f7473747261702d6c6f676f2d736861646f772e706e67",
-          class: "h-24",
         },
-        { src: "https://miro.medium.com/max/800/1*mUISLg4ghf6QYT_f1-cnlg.png" },
+        {
+          src: "https://miro.medium.com/max/800/1*mUISLg4ghf6QYT_f1-cnlg.png",
+          extraClasses: "col-span-full",
+        },
       ],
     },
     {
-      extraClasses: "bg-black -top-12 -left-12 flex-col",
+      extraClasses: "bg-black -top-12 -left-12 grid-cols-2 grid-rows-2",
       images: [
         {
           src:
             "https://blog.knoldus.com/wp-content/uploads/2019/06/graphql.png",
-          class: "h-20",
         },
 
         {
+          src:
+            "https://user-images.githubusercontent.com/841294/53402609-b97a2180-39ba-11e9-8100-812bab86357c.png",
+        },
+  
+        {
           src: "media/postgraphile.png",
-          class: "h-12",
+          extraClasses: "col-span-full",
         },
       ],
     },
@@ -52,7 +55,7 @@
       images: [
         {
           src: "https://image.flaticon.com/icons/png/512/1183/1183672.png",
-          class: "h-32",
+          extraClasses: "h-32",
         },
       ],
     },
--- src/cards/one_card.svelte
+++ src/cards/one_card.svelte
@@ -4,6 +4,8 @@
 </script>
 
 <div
-  class="grid-cols-2 grid-row-2 absolute h-40 rounded-lg w-72 transform transition-all skew-x-12 -skew-y-12 {extraClasses}">
-  {#each images as ic}<img alt="Card" src={ic.src} class={ic.class} />{/each}
+  class="grid absolute h-40 rounded-lg w-72 transform transition-all skew-x-12 -skew-y-12 {extraClasses}">
+  {#each images as ic}
+    <img alt="Card" src={ic.src} class="object-contain h-full place-self-center {ic.extraClasses ? ic.extraClasses : ""}" />
+  {/each}
 </div>
