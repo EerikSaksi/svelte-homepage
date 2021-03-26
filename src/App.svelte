f0ed59f324d56e87b1ea8dc1016d702b80bb7130
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-26 22:23:56 +0200

    Fixed layout issues that reintroduced

--- src/App.svelte
+++ src/App.svelte
@@ -2,17 +2,19 @@
   import Tailwind from "./Tailwind.svelte";
   import LeftHandIcons from "./icons/left_hand.svelte";
   import RightHandIcons from "./icons/right_hand.svelte";
-  import Timeline from "./timeline/bar.svelte";
+  import Timeline from "./timeline/explanation.svelte";
 </script>
 
 <Tailwind />
 <div class="flex flex-col h-screen md:container md:mx-auto">
-  <div class="flex flex-row justify-center flex-1">
+  <div class="flex justify-center flex-1">
     <LeftHandIcons />
-    <img
-      class="h-72"
-      alt="Cartoon Eerik"
-      src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairFrizzle&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light" />
+    <div class="flex flex-col justify-evenly">
+      <img
+        class="h-72"
+        alt="Cartoon Eerik"
+        src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairFrizzle&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light" />
+    </div>
     <RightHandIcons />
   </div>
   <div class="flex-1">
