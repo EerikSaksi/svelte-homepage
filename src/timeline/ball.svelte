b13c5e284261dad78391941c6d61fefffff886fa
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-26 23:25:03 +0200

    Fixed box margins

--- src/timeline/ball.svelte
+++ src/timeline/ball.svelte
@@ -4,15 +4,13 @@
 
 <div class="flex flex-row">
   <div class="relative flex flex-row items-center justify-center">
-    <div class="absolute flex">
+    <div class="absolute flex {isRight ?  '-left-4' : '-right-4'}">
       {#if isRight}
         <div
           class="w-6 h-6 rounded-full bg-gradient-to-r from-green-200 to-green-500" />
-        <div>
-          <p>2022</p>
-        </div>
+        <p class="ml-1">2018</p>
       {:else}
-        <p>2020</p>
+        <p class="mr-1">2018</p>
         <div
           class="w-6 h-6 rounded-full bg-gradient-to-r from-green-500 to-green-200" />
       {/if}
