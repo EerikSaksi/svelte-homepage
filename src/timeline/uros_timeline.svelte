bb8ce692beb82f6689e778a148d2670947210b30
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-27 12:29:48 +0200

    Testing no width

---
+++ src/timeline/uros_timeline.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Timeline from "./timeline.svelte";
+</script>
+
+<Timeline
+  leftShift = "left-1/4"
+  dates = {["Jun 2019", "Aug 2019"]}
+  widths = "w-2/72 md:w-1/24"        
+  explanationText="Software Engineer Intern"
+  explanationOnTop={false}
+  src="media/uros.webp"
+  colors={['from-red-200', 'via-red-500', 'to-red-200', 'from-red-500', 'to-red-500', 'ring-red-500', 'text-red-500']} />
