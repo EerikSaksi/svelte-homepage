2c7dc5dadf4944c28bd13698edef82c1faad7044
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-30 22:49:02 +0300

    Added animations

--- src/carousel/slide.svelte
+++ src/carousel/slide.svelte
@@ -10,7 +10,7 @@
 </style>
 
 <div
-  style="transform: translate({(dist - 0.5) * 100}%) scale({5 / (5 + Math.abs(dist))}); "
+  style="transform: translate({(dist - 0.5) * 100}%) scale({5 / (5 + Math.abs(dist))}); filter: blur({Math.abs(dist) * 6}px)"
   class="absolute inset-0 flex items-center justify-center text-5xl text-white w-11/12 md:w-1/3  rounded-xl transition-all ease-in-out duration-1000 transform slide left-1/2 {Math.abs(dist) == 2 ? 'invisible' : Math.abs(dist) == 1 ? 'shadow-md backdrop-filter-blur' : 'shadow-lg'} ease-in-out mb-4 animations-fixed">
   <slot />
 </div>
