fa4adc483b450cd67cb92aae2fc6ea22d146aa81
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-30 12:59:36 +0300

    Works

--- src/carousel/carousel.svelte
+++ src/carousel/carousel.svelte
@@ -3,22 +3,26 @@
   import Slide from "./slide.svelte";
   import TechnologyGrid from "./technology_grid.svelte";
 
-  let activeIndex = 3;
+  const convertIndex = (index) => {
+    if (index < 0) {
+      return 5 + index;
+    }
+    return index % 5;
+  };
+
+  let activeIndex = 0;
   export let technologies;
   export let distances = Array(5);
 
   async function nextSlide() {
     activeIndex = (activeIndex + 1) % 5;
-    distances = Array(5).fill(0).map((_, i) => {
-      const nonLooped = activeIndex - i
-      const looped = (5 - activeIndex) + i
-      console.log({nonLooped, looped})
-      if (Math.abs(nonLooped) < Math.abs(looped)){
-        return nonLooped
-      }
-      return looped
-    })
-    console.log(distances)
+    const tempDistances = Array(5).fill(0);
+    tempDistances[convertIndex(activeIndex - 1)] = -1;
+    tempDistances[convertIndex(activeIndex - 2)] = -2;
+    tempDistances[convertIndex(activeIndex + 1)] = 1;
+    tempDistances[convertIndex(activeIndex + 2)] = 2;
+    distances = tempDistances;
+    console.log(distances);
   }
   onMount(() => {
     const interval = setInterval(() => nextSlide(), 1000);
@@ -28,9 +32,9 @@
 
 <div class="relative w-full h-64 rounded-xl">
   {#each technologies as technology, i}
-    {#if distances[i] != 5}
+    {#if Math.abs(distances[i]) != 2}
       <Slide dist={distances[i]}>
-        <TechnologyGrid/>
+        <TechnologyGrid />
       </Slide>
     {/if}
   {/each}
--- src/carousel/slide.svelte
+++ src/carousel/slide.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  style="transform: translate({dist}00%) scale({3 / (Math.abs(dist) + 3)});"
-  class="absolute inset-0 flex items-center justify-center h-64 text-5xl text-white border rounded-xl w-96 transition-all ease-in-out duration-1000 transform slide ">
+  style="transform: translate({dist}00%)"
+  class="absolute inset-0 flex items-center justify-center text-5xl text-white border w-96 h-128 rounded-xl transition-all ease-in-out duration-1000 transform slide">
   <slot />
 </div>
