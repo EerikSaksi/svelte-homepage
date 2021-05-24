fea2a9a85c26d29ed71effbb73c0b6bc5fb4b723
Eerik Saksi <saksi.eerik@gmail.com>
2021-05-24 16:15:35 +0300

    New icons

--- src/icons/left_hand.svelte
+++ src/icons/left_hand.svelte
@@ -1,56 +1,57 @@
-<a class="inline-flex items-center text-start" href="tel:+44 7519 698 702">
-  Mobile
-  <svg
-    class="h-6 ml-1 md:h-12"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor">
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-  </svg>
-</a>
-<a
-  class="inline-flex items-center text-start"
-  href="mailto:saksi.eerik@gmail.com">
-  Email
-  <svg
-    class="h-6 ml-1 md:h-12"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor">
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-  </svg>
-</a>
+<div class="flex flex-row text-lg">
+  <a href="tel:+44 7519 698 702" class="inline-flex m-1">
+    <svg
+      class="h-8"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+    </svg>
+    Mobile
+  </a>
+  <a href="mailto:saksi.eerik@gmail.com" class="inline-flex m-1">
+    <svg
+      class="h-8 mr-1"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor">
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+    </svg>
+    Email
+  </a>
 
-<a class="inline-flex items-center " href="https://github.com/EerikSaksi">
-  <img alt="Github" class="w-6 mr-2 md:w-12" src="media/git.webp" />
-  Github
-</a>
+  <a href="https://github.com/EerikSaksi" class="inline-flex m-1">
+    <img alt="Github" src="media/git.webp" class="h-8 mr-2" />
+    Github
+  </a>
 
-<a
-  class="inline-flex items-center text-end"
-  href="https://drive.google.com/uc?export=download&id=1ZkthduWcQRQuJ70Ma_PTB11x7ePEzwM7"
-  download>
-  <svg
-    class="h-8 md:w-14 md:h-14"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor">
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-  </svg>
-  Resume
-</a>
+  <a
+    href="https://drive.google.com/uc?export=download&id=1ZkthduWcQRQuJ70Ma_PTB11x7ePEzwM7"
+    download
+    class="inline-flex m-1"
+  >
+    <svg
+      class="h-8"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor">
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+    </svg>
+    Resume
+  </a>
+</div>
